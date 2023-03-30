8845d7b9f30603d8c5170f3a5b0ad56fedd744c6
nowamey <104644214+nowamey@users.noreply.github.com>
2023-03-30 13:06:02 -0400

    sequelize now dropped, working on routes todb

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+
 import {v4 as uuidv4} from 'uuid';
 
 const app = express();
@@ -10,43 +11,53 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
-require("C:/Users/nowam/Documents/GitHub/capstoneweedwarriors/src/routes/dbroutes.js")(app);
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'mydb'
+})
+connection.connect()
 
-const db = require("./models");
-db.sequelize.sync()
-  .then(() => {
-    console.log("Synced db.");
-  })
-  .catch((err) => {
-    console.log("Failed to sync db: " + err.message);
-  });
+/*module.exports = app => {
+  const weedData = require("../controllers/tutorial.controller.js");
 
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and re-sync db.");
-});
+  var router = require("express").Router();
+
+  // Create a new Tutorial
+  router.post("/", weedData.create);
+
+  // Retrieve all Tutorials
+  router.get("/", weedData.findAll);
+
+  // Retrieve all published Tutorials
+  router.get("/published", WeedData.findAllPublished);
+
+  // Retrieve a single Tutorial with id
+  router.get("/:id", WeedData.findOne);
+
+  // Update a Tutorial with id
+  router.put("/:id", WeedData.update);
+
+  // Delete a Tutorial with id
+  router.delete("/:id", WeedData.delete);
+
+  // Delete all Tutorials
+  router.delete("/", WeedData.deleteAll);
+
+  app.use('/api/tutorials', router);
+};*/
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
+
 app.listen(process.env.PORT, () =>
     console.log(`listening at http://localhost:${process.env.PORT}`),
 );
-/*app.post('/messages', (req, res) => {
-  
-  console.log(req.body.text);
-  const id = uuidv4();
-  const text = req.body.text;
-  const message = {
-    id,
-    text
-  };
-
-  messages[id] = message;
-
-  return res.send(message);
-});
-
-app.get('/messages',(req,res) => {
-    return res.send(Object.values(messages));
-});
-app.get('/messages/:messageId', (req, res) => {
-    return res.send(messages[req.params.messageId]);
-  });
-*/ 
 
